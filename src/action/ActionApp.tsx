dc830fc469538fc82ecb501c8b4bfa766f56aacd
ram-shankar <ramshankar.choudhary@apty.io>
2024-11-17 19:30:14 +0530

    feat: bootstrap extension

---
+++ src/action/ActionApp.tsx
@@ -0,0 +1,12 @@
+
+function ActionApp() {
+  return (
+    <section className="container mx-auto p-5 fixed inset-0">
+      <div className="w-full h-full flex flex-col">
+        <h1>Action App</h1>
+      </div>
+    </section>
+  )
+}
+
+export default ActionApp
